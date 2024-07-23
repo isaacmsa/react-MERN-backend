94822978cc4305a38c421c9522bc4b708d216421
Isaac Mendoza <isaac.mendozasa@gmail.com>
2024-07-22 21:18:11 -0400

    added react app to project

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /* Configuracion inicial de express */
+const path = require('path')
 const express = require('express')
 require('dotenv').config()
 const { dbConnection } = require('./database/config')
@@ -24,6 +25,10 @@ app.use(express.json())
 /* Rutas */
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/events', require('./routes/events'))
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
 
 /* Escuchando peticiones */
 app.listen(process.env.PORT, () => {
